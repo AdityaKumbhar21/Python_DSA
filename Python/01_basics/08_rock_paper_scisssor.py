6f034b93c765618230fd72d19002cf865ea4bbc1
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-03-31 08:05:19 +0530

    added restart functionality

--- Python/01_basics/08_rock_paper_scisssor.py
+++ Python/01_basics/08_rock_paper_scisssor.py
@@ -15,41 +15,46 @@ if(is_game_on):
     score_comp = 0
     score_user = 0
 
-
-    comp_value = random.randint(1,3)
-    user_value = int(input("Enter your choice: "))
-    print()
-
-    if(comp_value == 1 and user_value == 2):
-        score_comp = score_comp + 1
-        print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
-        print("Computer wins!")
+    user_choice = 1
+    while(user_choice == 1):
+        comp_value = random.randint(1,3)
+        user_value = int(input("Enter your choice: "))
         print()
+
+        if(comp_value == 1 and user_value == 2):
+            score_comp = score_comp + 1
+            print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
+            print("Computer wins!")
+            print()
+            
+
+        elif comp_value == 2 and user_value == 3:
+            score_comp = score_comp + 1
+            
+            print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
+            print("Computer wins!")
+            print()
         
 
-    elif comp_value == 2 and user_value == 3:
-        score_comp = score_comp + 1
-        
-        print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
-        print("Computer wins!")
-        print()
-        
-
-    elif comp_value == 3 and user_value == 1:
-        score_comp = score_comp + 1
-        
-        print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
-        print("Computer wins!")
-        print()
+        elif comp_value == 3 and user_value == 1:
+            score_comp = score_comp + 1
+            
+            print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
+            print("Computer wins!")
+            print()
         
 
-    else:
-        score_user = score_user + 1
-        print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
-        print("User wins!")
-        print()
+        else:
+            score_user = score_user + 1
+            print("Score: \nComp: ",score_comp,"\nUser: ",score_user)
+            print("User wins!")
+            print()
         
+        user_choice = int(input("Press 1 to restart the game: "))
+    
 
     print("Game ended")
 
-
+else:
+    print("Game cannot be started")
+    exit()
