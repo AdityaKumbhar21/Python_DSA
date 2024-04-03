b29ca2ee77894a130f0eb14a66635a200ece4c51
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-03 10:57:55 +0530

    Add factorial_numbers function to calculate factorial numbers up to given integer

--- Recursion/03_factorial.py
+++ Recursion/03_factorial.py
@@ -1,7 +1,25 @@
+from typing import *
+
 def factorial(n):
     if n == 1:
         return 1
     else:
         return n*factorial(n-1)
+
+# You are given an integer ’n’.
+# Your task is to return a sorted array (in increasing order) containing all the factorial numbers which are less than or equal to ‘n’.
+    
+def factorial_numbers(n) -> List[int]:
+    num = 1
+    fact = 1
+    ans = []
+    while fact<=n:
+        ans.append(fact)
+        num+=1
+        fact *= num
+    return ans
+
+    
+
 
 print(factorial(6))
