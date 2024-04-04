692897c50855ddfccc0232013e7ece502246f9c9
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-04 09:50:48 +0530

    Refactor palindrome string check function

--- Recursion/05_palindrome_string.py
+++ Recursion/05_palindrome_string.py
@@ -14,4 +14,28 @@ def isPalindrome(string: str) -> bool:
     size = len(string)
     return palindromeHelper(0,size-1, string)
     
+
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+def recHelper(start, s):
+        if start >= len(s) //2:
+            return True
+        if(s[start] != s[len(s)-start-1]):
+            return False
+        return recHelper(start+1, s) 
+
+
+
+def isPalindrome(s):
+        s = s.lower()
+        s = ''.join(e for e in s if e.isalnum())
+        if(len(s)<1):
+            return True
+        
+        return recHelper(0, s)
+
+
 print(isPalindrome("abcd"))
