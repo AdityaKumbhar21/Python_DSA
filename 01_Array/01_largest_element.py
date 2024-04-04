8856bae20864152823776f928cad97350ae6ee36
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-04 10:47:47 +0530

    Add largest_element and largest_element_optimal functions to find the largest element in an array

---
+++ 01_Array/01_largest_element.py
@@ -0,0 +1,23 @@
+
+
+
+# # Brute force
+def largest_element(arr):
+    arr.sort()
+    return arr[-1]
+
+#Optimal
+
+def largest_element_optimal(arr):
+    n = len(arr)
+    largest = arr[0]
+    for i in range(1,n):
+        if(arr[i] > largest):
+            largest = arr[i]
+    return largest
+
+
+
+my_arr = [8,10,5,7,9]
+print(largest_element(my_arr))
+print(largest_element_optimal(my_arr))
