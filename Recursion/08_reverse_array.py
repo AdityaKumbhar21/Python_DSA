8523c4d1e6cd22e7698c599bd001d04057c1b1f1
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-04 09:50:39 +0530

    Add recursive function to reverse an array

---
+++ Recursion/08_reverse_array.py
@@ -0,0 +1,29 @@
+from typing import *
+
+def swap(a,b):
+    temp = a
+    a = b
+    b = temp
+
+# ans = []
+# def reverseArray(n:int, nums: List[int],ind):
+#     if ind < 0:
+#         return ans 
+#     else:
+#        ans.append(nums[ind])    
+#        return reverseHelper(n, nums, ind-1)
+
+
+# Without returning the list
+
+def reverseArray(n:int, nums: List[int],s, e):
+    if s>e:
+        return 
+    else:
+       nums[s], nums[e] = nums[e], nums[s]  
+       reverseArray(n,nums, s+1, e-1)   
+        
+my_list = [1,2,3,4]
+n = len(my_list)
+reverseArray(n, my_list, 0, n-1)
+print(my_list)
