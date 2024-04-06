b1e4ac600af0aa140d44095c4f01b3b0aeb7c8f0
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-06 10:28:11 +0530

    Add code for removing duplicates from an array

---
+++ 01_Array/05_remove_duplicates.py
@@ -0,0 +1,33 @@
+
+def removeDuplicates_brute(nums):
+        temp = [nums[0]]
+      
+        n = len(nums)
+        for i in range(1,n):
+            if nums[i] != nums[i-1]:
+                temp.append(nums[i])
+        
+        for i in range(0, len(temp)):
+         nums[i] = temp[i]
+        
+        return len(temp)
+
+
+def removeDuplicates_opt(nums):
+    left = 1
+    n = len(nums)
+    for right in range(1,n):
+            if nums[right] != nums[right-1]:
+                nums[left] = nums[right]
+                left += 1
+                print(left)
+    return left
+        
+
+
+
+
+
+arr = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates_brute(arr))
+print(removeDuplicates_opt(arr))
