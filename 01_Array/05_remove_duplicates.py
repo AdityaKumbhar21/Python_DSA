e8bfdc5a41a71351abc068b96ca2cc77be09e018
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-06 10:29:52 +0530

    Refactor removeDuplicates_brute function to improve efficiency

--- 01_Array/05_remove_duplicates.py
+++ 01_Array/05_remove_duplicates.py
@@ -4,7 +4,7 @@ def removeDuplicates_brute(nums):
       
         n = len(nums)
         for i in range(1,n):
-            if nums[i] != nums[i-1]:
+            if nums[i] != nums[i-1]:  # for checking the first occurence of the element
                 temp.append(nums[i])
         
         for i in range(0, len(temp)):
@@ -23,10 +23,6 @@ def removeDuplicates_opt(nums):
                 print(left)
     return left
         
-
-
-
-
 
 arr = [0,0,1,1,1,2,2,3,3,4]
 print(removeDuplicates_brute(arr))
