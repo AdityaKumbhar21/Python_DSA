47aa13dc85b3f972d4bb72f6de5e0bd719ac943b
Aditya Kumbhar <adityakumbhar915@gmail.com>
2024-04-14 10:00:38 +0530

    Refactor longestConsecative function to longestConsecative_better and add print function

--- 01_Array/11_longest_consecative_subsequence.py
+++ 01_Array/11_longest_consecative_subsequence.py
@@ -13,7 +13,7 @@ def longestConsecutive(nums):
         
        return longest_seq
 
-def longestConsecative(nums):
+def longestConsecative_better(nums):
     numsSet = set(nums)
     longest_seq = 0
 
@@ -24,7 +24,32 @@ def longestConsecative(nums):
                 length += 1
             longest_seq = max(longest_seq, length)
         
-        return longest_seq
+    return longest_seq
+    
+
+#print the longest sequence elements
+def longestConsecative_better_print(nums):
+    numsSet = set(nums)
+    longest_seq = 0
+    ans  = []
+
+
+    for n in nums:
+        if (n-1) not in numsSet:
+            length = 0
+            curr_seq_elements = []  # keeping track of current sequence of consecutives
+            while(n + length) in numsSet:
+                length += 1
+                curr_seq_elements.append(n + length - 1)
+
+            if length > longest_seq:  # if the curr_seq is greater than any before then update the length and ans
+                longest_seq = length
+                ans = curr_seq_elements
+        
+    print(ans)
+    return longest_seq
 
 nums = [9,1,4,7,3,-1,0,5,8,-1,6]
 print(longestConsecutive(nums))
+print(longestConsecative_better(nums))
+print(longestConsecative_better_print(nums))
